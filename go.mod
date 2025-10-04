db0ea0a6136276b674a82b3500f5947bdadbae32
maxifly <maxifly@mail.ru>
2025-10-04 16:21:38 +0300

    add slog

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module imgserver
 
 go 1.21.3
+
+require github.com/natefinch/lumberjack v2.0.0+incompatible // indirect
--- internal/appimgserver/imgserver.go
+++ internal/appimgserver/imgserver.go
@@ -3,8 +3,9 @@ package appimageserver
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/natefinch/lumberjack"
 	"imgserver/internal/pkg/mylogger"
-	"log"
+	"log/slog"
 	"os"
 )
 
@@ -12,7 +13,7 @@ const FILE_PATH_OPTIONS = "/data/options.json"
 
 type ImgSrv struct {
 	options ApplOptions
-	logger  *mylogger.Logger
+	logger  *slog.Logger
 }
 
 type ApplOptions struct {
@@ -20,41 +21,88 @@ type ApplOptions struct {
 }
 
 func NewImgSrv(port string) *ImgSrv {
-	logFormat := log.Ldate | log.Ltime | log.Lshortfile
-
-	debugLog := log.New(mylogger.NewNullWriter(), "DEBUG\t", logFormat)
-	infoLog := log.New(mylogger.NewNullWriter(), "INFO\t", logFormat)
-	errorLog := log.New(os.Stderr, "ERROR\t", logFormat)
-	isDebudDisable := true
-
-	//scheduleLogLevel := gocron.LogLevelWarn
-
-	// Test log messages
 
 	options, err := readOptions()
 	if err != nil {
 		panic(fmt.Sprintf("Can not read Options: %v", err))
 	}
 
+	// Настройка обработчика для записи в файл с ротацией
+	fileLogger := &lumberjack.Logger{
+		Filename:   "app.log", // Имя файла логов
+		MaxSize:    100,       // Максимальный размер файла в мегабайтах
+		MaxBackups: 3,         // Максимальное количество старых файлов для хранения
+		MaxAge:     28,        // Максимальный возраст файла в днях
+		Compress:   true,      // Сжимать ли старые файлы в формате gzip
+	}
+
+	var logLevel = slog.LevelInfo
+
 	if options.LogLevel == "DEBUG" {
-		debugLog = log.New(os.Stdout, "DEBUG\t", logFormat)
-		infoLog = log.New(os.Stdout, "INFO\t", logFormat)
-		isDebudDisable = false
-
+		logLevel = slog.LevelDebug
+	} else if options.LogLevel == "INFO" {
+		logLevel = slog.LevelInfo
+	} else if options.LogLevel == "WARNING" {
+		logLevel = slog.LevelWarn
+	} else {
+		logLevel = slog.LevelError
 	}
-	if options.LogLevel == "INFO" {
-		infoLog = log.New(os.Stdout, "INFO\t", logFormat)
-	}
 
-	debugLog.Println("hello")
-	infoLog.Println("hello")
-	errorLog.Println("hello")
+	fileHandler := slog.NewTextHandler(fileLogger, &slog.HandlerOptions{
+		Level: logLevel, AddSource: true,
+	})
 
-	// Инициализируем новую структуру с зависимостями приложения.
-	logger := mylogger.New(errorLog, infoLog, debugLog)
-	if isDebudDisable {
-		logger.DisableDebug()
-	}
+	// Настройка обработчика для вывода на экран
+	consoleHandler := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level: logLevel, AddSource: true,
+	})
+
+	// Объединение обработчиков в группу
+	group := mylogger.NewMultiHandler(fileHandler, consoleHandler)
+
+	// Создание логгера с группой обработчиков
+	logger := slog.New(group)
+
+	logger.Info("Application started", slog.String("status", "OK"))
+	logger.Debug("This is a debug message", slog.String("detail", "additional info"))
+	logger.Warn("This is a warning message")
+	logger.Error("This is an error message", slog.String("error", "something went wrong"))
+
+	//logFormat := log.Ldate | log.Ltime | log.Lshortfile
+
+	//debugLog := log.New(mylogger.NewNullWriter(), "DEBUG\t", logFormat)
+	//infoLog := log.New(mylogger.NewNullWriter(), "INFO\t", logFormat)
+	//errorLog := log.New(os.Stderr, "ERROR\t", logFormat)
+	//isDebudDisable := true
+
+	//scheduleLogLevel := gocron.LogLevelWarn
+
+	// Test log messages
+
+	//options, err := readOptions()
+	//if err != nil {
+	//	panic(fmt.Sprintf("Can not read Options: %v", err))
+	//}
+	//
+	//if options.LogLevel == "DEBUG" {
+	//	debugLog = log.New(os.Stdout, "DEBUG\t", logFormat)
+	//	infoLog = log.New(os.Stdout, "INFO\t", logFormat)
+	//	isDebudDisable = false
+	//
+	//}
+	//if options.LogLevel == "INFO" {
+	//	infoLog = log.New(os.Stdout, "INFO\t", logFormat)
+	//}
+	//
+	//debugLog.Println("hello")
+	//infoLog.Println("hello")
+	//errorLog.Println("hello")
+	//
+	//// Инициализируем новую структуру с зависимостями приложения.
+	//logger := mylogger.New(errorLog, infoLog, debugLog)
+	//if isDebudDisable {
+	//	logger.DisableDebug()
+	//}
 
 	return &ImgSrv{
 		options: options,
