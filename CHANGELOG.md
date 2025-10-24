73bf48437780807cc5f6cc690b5f7a5a9381d7ab
maxifly <maxifly@mail.ru>
2025-10-24 17:57:29 +0300

    Add sleep periods

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,9 @@
 # Changelog
 ## 1.02.0
+  **ТРЕБУЕТСЯ ПРОШИТЬ РАМКУ**: версией 1.02
 
 - Add threshold for providers.  [#1](https://github.com/maxifly/ImageServer/issues/1)
+- Add sleep mode.  [#6](https://github.com/maxifly/ImageServer/issues/6)
 
 
 ## 1.1
--- src/internal/pkg/opermanager/opermanager.go
+++ src/internal/pkg/opermanager/opermanager.go
@@ -2,10 +2,13 @@ package opermanager
 
 import (
 	"fmt"
+	"image"
+	"image/jpeg"
 	"imgserver/internal/pkg/actioner"
 	"imgserver/internal/pkg/dirmanager"
 	"log/slog"
 	"math/rand"
+	"os"
 	"path/filepath"
 	"strconv"
 	"strings"
@@ -23,6 +26,8 @@ const (
 	OldPicture                      // 1
 )
 
+const BLACK_FILE_NAME = "black.jpeg"
+
 type Status string
 
 // Определяем константы для статусов
@@ -33,6 +38,11 @@ const (
 	StatusError   Status = "error"
 )
 
+type SleepTime struct {
+	TimeRange      *TimeRange `yaml:"time_range"`
+	BlackImageMode bool       `yaml:"black_image_mode"`
+}
+
 type OperMngr struct {
 	directoryPath      string
 	pendingOperations  *cache.Cache
@@ -40,9 +50,11 @@ type OperMngr struct {
 	logger             *slog.Logger
 	imageProviders     []*ImageProvider
 	//ydArt              *ydart.YdArt
-	dirManager *dirmanager.DirManager
-	idMutex    *IdMutex
-	actioner   *actioner.Actioner
+	dirManager      *dirmanager.DirManager
+	idMutex         *IdMutex
+	actioner        *actioner.Actioner
+	sleepTimes      []*SleepTime
+	imageParameters *ImageParameters
 }
 type OperStatus struct {
 	Status Status
@@ -59,6 +71,8 @@ type Operation struct {
 }
 
 func NewOperMngr(directoryPath string, thresholdMinutes int,
+	parameters *ImageParameters,
+	sleepTimes []*SleepTime,
 	dirManager *dirmanager.DirManager,
 	logger *slog.Logger) *OperMngr {
 
@@ -73,6 +87,8 @@ func NewOperMngr(directoryPath string, thresholdMinutes int,
 		logger:             logger,
 		idMutex:            NewIdMutex(),
 		actioner:           actioner.NewActioner(thresholdMinutes, time.Minute),
+		sleepTimes:         sleepTimes,
+		imageParameters:    parameters,
 	}
 	return &operMng
 }
@@ -85,6 +101,18 @@ func (op *OperMngr) Start() error {
 	if len(op.imageProviders) == 0 {
 		return fmt.Errorf("no image providers found")
 	}
+	err := op.createBlackJPEGSafe(op.imageParameters.Weight, op.imageParameters.Height, BLACK_FILE_NAME)
+	if err != nil {
+		return err
+	}
+
+	for _, st := range op.sleepTimes {
+		_, err = st.TimeRange.IsWithinRangeInclusive(time.Now())
+		if err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
 
@@ -137,6 +165,18 @@ func (op *OperMngr) chooseImageProvider() *ImageProvider {
 func (op *OperMngr) startAutoOperation() (string, error) {
 	op.logger.Info("Start auto operation")
 	now := time.Now()
+
+	st := op.checkSleepTime(now)
+
+	// Сейчас период сна. Посмотрим что надо сделать.
+	if st != nil {
+		if st.BlackImageMode {
+			return op.startBlackPictureOperation()
+		} else {
+			return op.startOldPictureOperation()
+		}
+	}
+
 	if op.actioner.ThresholdOut(now) {
 		op.logger.Debug("Threshold")
 		// Внешний провайдер давно не вызывался
@@ -160,9 +200,23 @@ func (op *OperMngr) startAutoOperation() (string, error) {
 	}
 }
 
+func (op *OperMngr) startBlackPictureOperation() (string, error) {
+	op.logger.Info("Start black picture operation")
+	return op.startGetOldPictureFromLocalStorageOperation(true)
+}
 func (op *OperMngr) startOldPictureOperation() (string, error) {
 	op.logger.Info("Start old picture operation")
-	file := op.dirManager.GetRandomFile()
+	return op.startGetOldPictureFromLocalStorageOperation(false)
+}
+
+func (op *OperMngr) startGetOldPictureFromLocalStorageOperation(getBlackPicture bool) (string, error) {
+
+	var file string
+	if getBlackPicture {
+		file = BLACK_FILE_NAME
+	} else {
+		file = op.dirManager.GetRandomFile()
+	}
 	op.logger.Info("Start old picture operation", "file", file)
 	operation := Operation{
 		Id:         op.generateId(),
@@ -281,6 +335,87 @@ func (op *OperMngr) CheckPendingOperations() {
 			op.logger.Error("error when check pending operation", "error", err, "id", id)
 		}
 	}
+}
+
+func (op *OperMngr) checkSleepTime(now time.Time) *SleepTime {
+
+	for _, st := range op.sleepTimes {
+		inclusive, err := st.TimeRange.IsWithinRangeInclusive(now)
+		if err != nil {
+			op.logger.Error("Get time range error", "error", err)
+		}
+		if inclusive {
+			return st
+		}
+	}
+	return nil
+}
+
+func (op *OperMngr) createBlackJPEGSafe(width, height int, filename string) error {
+	// Проверяем существование файла
+	if _, err := os.Stat(filename); err == nil {
+		// Файл существует - удаляем его
+		if err := os.Remove(filename); err != nil {
+			op.logger.Error("Remove old image file failed", "error", err, "filename", filename)
+			return fmt.Errorf("can not delete file %s: %w", filename, err)
+		}
+		op.logger.Debug("Remove old image file success", "filename", filename)
+	} else if !os.IsNotExist(err) {
+		// Другая ошибка при проверке существования
+		op.logger.Error("Check image file failed", "error", err, "filename", filename)
+		return fmt.Errorf("check image file error: %w", err)
+	}
+
+	// Создаем новое изображение
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+
+	// Заполняем черным цветом
+	op.fillBlack24Bit(img)
+
+	file, err := os.Create(filename)
+	if err != nil {
+		op.logger.Error("Create image file failed", "error", err, "filename", filename)
+		return fmt.Errorf("create image file error: %w", err)
+	}
+	defer file.Close()
+
+	// Сохраняем как JPEG
+
+	jpegOptions := &jpeg.Options{
+		Quality: 100, // Качество сжатия
+		// Color model и bit depth будут автоматически определены из RGBA
+	}
+
+	if err := jpeg.Encode(file, img, jpegOptions); err != nil {
+		op.logger.Error("Create JPEG image file failed", "error", err, "filename", filename)
+		return fmt.Errorf("create JPEG image file error: %w", err)
+	}
+
+	op.logger.Debug("Create image file success", "filename", filename)
+	return nil
+}
+
+func (op *OperMngr) fillBlack24Bit(rgba *image.RGBA) {
+	bounds := rgba.Bounds()
+	pixels := bounds.Max.X * bounds.Max.Y
+
+	op.logger.Debug(fmt.Sprintf("Fill %d pixels black color...", pixels))
+
+	// Устанавливаем все пиксели в черный цвет
+
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		rowStart := y * rgba.Stride
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			pixelPos := rowStart + x*4
+
+			rgba.Pix[pixelPos] = 0     // R = 0 (черный)
+			rgba.Pix[pixelPos+1] = 0   // G = 0 (черный)
+			rgba.Pix[pixelPos+2] = 0   // B = 0 (черный)
+			rgba.Pix[pixelPos+3] = 255 // A = 255 (непрозрачный)
+		}
+	}
+
+	op.logger.Debug("Fill pixels completed")
 }
 
 func (op *OperMngr) generateId() string {
